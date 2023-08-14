a361b4ab03e73b1e107f909a0167c481ea1a1391
Czar-dev-2 <56700690+Czar-dev-2@users.noreply.github.com>
2023-08-14 19:26:53 +0900

    coming soon

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import * as THREE from "three";
+
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
